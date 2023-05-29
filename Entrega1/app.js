c5e0bc9588a579e0e14f2f72138cd3447db35c1a
fernando reyes <reyes.fernando88@gmail.com>
2023-05-28 22:02:33 -0400

    Carpeta Entrega1

---
+++ Entrega1/app.js
@@ -0,0 +1,8 @@
+let nombre = prompt('Ingresa tu nombre');
+let Nacimiento = prompt('¿En que año naciste?');
+
+const Actual = 2023;
+
+let edad = Actual - Nacimiento;
+
+alert(nonmbre + ', tienes' + edad + 'años');
